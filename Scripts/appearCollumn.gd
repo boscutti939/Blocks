deb4460ea9de8ba9553ed9bb0893aad7128b9f0b
boscutti939 <tboscutti939@gmail.com>
2020-11-24 11:16:52 +0800

    another bug fix

--- Scripts/appearCollumn.gd
+++ Scripts/appearCollumn.gd
@@ -8,13 +8,13 @@ onready var global = $"/root/Global";
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, 0);
+	$Tween.start();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$appearSound.pitch_scale = global.timescale;
 	position.y = get_node("/root/field/sceneCamera").position.y;
-	modulate = Color(1, 1, 1, $visibleTimer.time_left / $visibleTimer.wait_time)
 
 func _on_Timer_timeout():
 	queue_free()
---
+++ Scripts/appearCollumn/Tween.gd
@@ -0,0 +1,17 @@
+extends Tween
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var global = $"/root/Global";
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	playback_speed = global.timescale;
